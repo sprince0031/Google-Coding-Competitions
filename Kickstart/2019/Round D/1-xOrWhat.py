0af49ebbdd8d6c6d1e7c6ee526bd0578a0d7802f
sprince0031 <siddharthprince31@gmail.com>
2019-08-02 00:43:37 +0530

    discovered pascal's triangle angle for problem xOrWhat

--- Kickstart/2019/Round D/1-xOrWhat.py
+++ Kickstart/2019/Round D/1-xOrWhat.py
@@ -1,23 +1,3 @@
-# def powSet(A, N):
-#     power = []
-#     for i in range(0, 1<<N):
-#         subset = []
-#         for j in range(0, N):
-#             if (1<<j) & i:
-#                 subset.append(A[j])
-#         power.append(subset)
-#     return power[1:]
-
-def window(L, size):
-    it = iter(L)
-    win = [it.__next__() for cnt in range(size)]
-    return win, it
-def subsequentWindows(win, it):
-    for e in it: # Subsequent windows
-        win[:-1] = win[1:]
-        win[-1] = e
-        return win
-
 def xor_sum(A, N):
     if N > 1:
         # print(A)
@@ -39,55 +19,95 @@ def  countSetBitsAndReturnIfEven(n):
     else:
         return -1
 
+# T = int(input())
+# for testcase in range(1, T+1):
+#     N, Q = map(int, input().split())
+#     A = list(map(int, input().split()))
+#     ans = ''
+#     for modification in range(0, Q):
+#         Pi, Vi = map(int, input().split())
+#         A[Pi] = Vi
+#         for length in range(N, 0, -1):
+#             print(length)
+#             for e in range(0, N-length):
+#                 print(e)
+#                 reqList, it = window(A, length)
+#                 print(reqList, it)
+#                 if countSetBitsAndReturnIfEven(xor_sum(reqList, len(reqList))) is not -1:
+#                     ans += "{} ".format(length)
+#                     continue
+#                 else:
+#                     restOfList = []
+#                     for w in range(N-length-1):
+#                        reqList = subsequentWindows(A, it) 
+#                        if countSetBitsAndReturnIfEven(xor_sum(reqList, len(reqList))) is not -1:
+#                             ans += "{} ".format(length)
+#                             continue
+#                 if len(ans) // 2 == modification + 1:
+#                     continue
+
+#             if len(ans) // 2 == modification + 1:
+#                 continue
+                    
+            
+
+
+#         # powSetOfA = powSet(A, N)
+#         # # print(powSetOfA, len(powSetOfA))
+#         # # xor_sums = [[xor_sum(i, len(i)), i] for i in powSetOfA]
+#         # # print(xor_sums, len(xor_sums))
+#         # xor_even_max = 0
+#         # for i in powSetOfA:
+#         #     xorSum = xor_sum(i, len(i))
+#         #     print(xorSum, i)
+#         #     if countSetBitsAndReturnIfEven(xorSum) is not -1:
+#         #         if len(i) > xor_even_max:
+#         #             xor_even_max = len(i)
+#         #         else:
+#         #             continue
+            
+#         # # xor_even_list = [countSetBitsAndReturnIfEven(i[0]) for i in xor_sums] # if countSetBitsAndReturnIfEven(i[0]) is not -1
+#         # # print(xor_even_list, len(xor_even_list))
+#         # ans += "{} ".format(xor_even_max)
+#     print("Case #{}: {}".format(testcase, ans[:len(ans)]))
+
+def generatePascalTriangleRow(n):
+    pascalRow = [1]
+    element = 1
+    # if n%2 == 0:
+    rangeToCalculate = n//2
+    # else:
+
+    for i in range(0, rangeToCalculate):
+        element *= (n-i)/(i+1)
+        pascalRow.append(int(element))
+    if n % 2 != 0:
+        return pascalRow + pascalRow[:-(len(pascalRow)):-1]
+    else:
+        toBeReversed = pascalRow[-len(pascalRow)+1:-1]
+        return pascalRow + toBeReversed[::-1]
+
 T = int(input())
 for testcase in range(1, T+1):
     N, Q = map(int, input().split())
     A = list(map(int, input().split()))
     ans = ''
-    for modification in range(0, Q):
+    for modfication in range(0, Q):
         Pi, Vi = map(int, input().split())
         A[Pi] = Vi
-        for length in range(N, 0, -1):
-            print(length)
-            for e in range(0, N-length):
-                print(e)
-                reqList, it = window(A, length)
-                print(reqList, it)
-                if countSetBitsAndReturnIfEven(xor_sum(reqList, len(reqList))) is not -1:
-                    ans += "{} ".format(length)
-                    continue
-                else:
-                    restOfList = []
-                    for w in range(N-length-1):
-                       reqList = subsequentWindows(A, it) 
-                       if countSetBitsAndReturnIfEven(xor_sum(reqList, len(reqList))) is not -1:
-                            ans += "{} ".format(length)
-                            continue
-                if len(ans) // 2 == modification + 1:
-                    continue
-
-            if len(ans) // 2 == modification + 1:
-                continue
-                    
-            
-
-
-        # powSetOfA = powSet(A, N)
-        # # print(powSetOfA, len(powSetOfA))
-        # # xor_sums = [[xor_sum(i, len(i)), i] for i in powSetOfA]
-        # # print(xor_sums, len(xor_sums))
-        # xor_even_max = 0
-        # for i in powSetOfA:
-        #     xorSum = xor_sum(i, len(i))
-        #     print(xorSum, i)
-        #     if countSetBitsAndReturnIfEven(xorSum) is not -1:
-        #         if len(i) > xor_even_max:
-        #             xor_even_max = len(i)
-        #         else:
-        #             continue
-            
-        # # xor_even_list = [countSetBitsAndReturnIfEven(i[0]) for i in xor_sums] # if countSetBitsAndReturnIfEven(i[0]) is not -1
-        # # print(xor_even_list, len(xor_even_list))
-        # ans += "{} ".format(xor_even_max)
+        pascalRow = generatePascalTriangleRow(N)
+        for possibilities in pascalRow:
+            for possibility in range(1, possibilities+1):
+                pass # have to complete 
+        # numberOfWindows, flag = 1, 0
+        # for windowSize in range(N, 0, -1):
+        #     for window in range(0, numberOfWindows):
+        #         if countSetBitsAndReturnIfEven(xor_sum(A[window:windowSize+window], windowSize)) is not -1:
+        #             ans += "{} ".format(windowSize)
+        #             flag = 1
+        #             break
+        #     if flag == 0:
+        #         numberOfWindows += 1
+        #     else:
+        #         break
     print("Case #{}: {}".format(testcase, ans[:len(ans)]))
-
